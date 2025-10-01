9343d4d55627583622f6ebf7bd941cf64d974bdd
Min Li <czl_min@hotmail.com>
2025-09-30 20:14:30 -0400

    init commit

---
+++ 15-day-learning.sql
@@ -0,0 +1,53 @@
+SET NOCOUNT ON;
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+-- Calendar: 2016-03-01 .. 2016-03-15
+
+
+with everyday_submission as 
+(
+    select hacker_id, submission_date, count(*) as submission_count
+    from Submissions 
+    where submission_date between '2016-03-01' and '2016-03-15'
+    group by hacker_id, submission_date 
+),
+everyday_max_submission as 
+(
+    select hacker_id, submission_date, 
+    row_number() over (partition by submission_date order by submission_count desc, hacker_id) as rn from everyday_submission 
+),
+dates as 
+(
+    select cast('2016-03-01' as date) as dt 
+    union all 
+    select dateadd(day, 1, dt)  
+    from dates 
+    where dt < cast('2016-03-15' as date)
+),
+active_submission as 
+(
+    select dt, count(*) as total_active from dates as d
+    cross apply (
+        select hacker_id from Submissions 
+        where submission_date between '2016-03-01' and d.dt
+        group by hacker_id 
+        having count(distinct submission_date) = datediff(day, '2016-03-01', d.dt) + 1
+    ) as count_submission 
+    group by d.dt 
+)
+select ems.submission_date, isnull(ac.total_active, 0) as total_active, ems.hacker_id, h.name from 
+dates d left join 
+active_submission ac on d.dt = ac.dt left join 
+everyday_max_submission ems 
+on ems.submission_date = ac.dt and ems.rn = 1
+left join hackers h 
+on ems.hacker_id = h.hacker_id
+order by ems.submission_date
+
+
+
+go
---
+++ interview.sql
@@ -0,0 +1,26 @@
+SET NOCOUNT ON;
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+with vs as (select s.challenge_id, sum(total_views) as total_views, sum(total_unique_views) as total_unique_views from view_stats s 
+group by s.challenge_id),
+ss as (select s.challenge_id, sum(total_submissions) as total_submissions, sum(total_accepted_submissions) as total_accepted_submissions from submission_stats s
+group by s.challenge_id)
+select c.contest_id, c.hacker_id, c.name, coalesce(sum(ss.total_submissions), 0) as total_submissions,  coalesce(sum(ss.total_accepted_submissions), 0) as total_accepted_submissions, coalesce(sum(vs.total_views), 0) as total_views, coalesce(sum(total_unique_views), 0) as total_unique_views
+from contests c 
+left join colleges col on c.contest_id = col.contest_id 
+left join challenges cha on col.college_id = cha.college_id 
+left join vs on cha.challenge_id = vs.challenge_id 
+left join ss on cha.challenge_id = ss.challenge_id 
+group by c.contest_id, c.hacker_id, c.name 
+having (coalesce(sum(ss.total_submissions), 0) + coalesce(sum(ss.total_accepted_submissions), 0) + 
+coalesce(sum(vs.total_views), 0) + coalesce(sum(total_unique_views), 0) )> 0 
+order by c.contest_id 
+
+
+
+
+go
