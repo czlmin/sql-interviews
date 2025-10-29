bec5f392090c7ba2c2940ab6eee4629f860fdc4b
run-ski <czl_min@hotmail.com>
2025-10-29 08:07:11 -0400

    Fix SQL script for finding prime numbers

--- find-prime-numbers.sql
+++ find-prime-numbers.sql
@@ -1,24 +1,28 @@
 declare @n as int = 100;
-declare @i as int
-declare @is_prime = 1
-declare @j as int
+declare @i as int = 2;
+declare @is_prime as int = 1;
+declare @j as int;
 
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[primes]') AND type in (N'U'))
+DROP TABLE [dbo].[primes]
 create table primes(prime_number int)
 while @i <= @n
 begin
-    @is_prime = 1
+    set @is_prime = 1
+    set @j = 2
     while @j < @i
     begin
         if @i % @j = 0
         begin
-            @is_prime = 0
+            set @is_prime = 0
             break
         end
+        set @j = @j + 1
     end
-    if @is_prime
+    if @is_prime = 1
     begin
         insert into primes(prime_number)
         values(@i)
     end
+    set @i = @i + 1
 end
-
